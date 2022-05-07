a6c4d6b0c8be0491c7e001dfce31c0dc1b15ab78
Лобова Дарья Сергеевна <94217366+darialobova@users.noreply.github.com>
2022-05-07 16:36:39 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,34 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+    T arr[100];
+    int first;
+    int last;
+  public:
+    TPQueue() : first(0) , last(0) {}
+    void push(T z1) {
+      if (last - first >= size) {
+        throw "Full!";
+      } else {
+        int z2 = last - 1;
+        while ((z2 >= first) && (arr[z2 % size].prior < z1.prior)) {
+          arr[(z2 + 1) % size] = arr[z2 % size];
+          z2--;
+        }
+        arr[(z2 + 1) % size] = z1;
+        last++;
+      }
+    }
+    T front() {
+      return arr[first % size];
+    }
+    T back() {
+      return arr[(last - 1) % size];
+    }
+    T pop() {
+      return arr[(first++) % size];
+    }
 };
 
 struct SYM {
